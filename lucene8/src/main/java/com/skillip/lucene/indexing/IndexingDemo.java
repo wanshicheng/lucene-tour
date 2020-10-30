0d5cc97830b39559755c71c429a3c6a9ddf4a84c
万仕诚 <wanshicheng@qq.com>
2020-10-30 23:12:05 +0800

    使用自定义Analyzer

--- lucene8/src/main/java/com/skillip/lucene/indexing/IndexingDemo.java
+++ lucene8/src/main/java/com/skillip/lucene/indexing/IndexingDemo.java
@@ -1,5 +1,7 @@
 package com.skillip.lucene.indexing;
 
+import com.skillip.lucene.analysis.DelimiterPayloadAnalyzer;
+import org.apache.lucene.analysis.payloads.FloatEncoder;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
@@ -12,14 +14,14 @@ import java.nio.file.Paths;
 public class IndexingDemo {
     public static void main(String[] args) throws IOException {
         MMapDirectory dir = new MMapDirectory(Paths.get("D:\\lucene\\index\\demo"));
-        IndexWriterConfig conf = new IndexWriterConfig(new StandardAnalyzer());
+        IndexWriterConfig conf = new IndexWriterConfig(new DelimiterPayloadAnalyzer('|', new FloatEncoder()));
         conf.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
         IndexWriter writer = new IndexWriter(dir, conf);
 
         Document doc = new Document();
         TextField name = new TextField("title", "Lucene Indexing Demo", Field.Store.YES);
         StoredField url = new StoredField("url", "http://skillip.com");
-        TextField content1 = new TextField("content", "Apache Lucene is an open source project available for free download.", Field.Store.YES);
+        TextField content1 = new TextField("content", "Apache Lucene|1.8 is an open source project available for free download.", Field.Store.YES);
         TextField content2 = new TextField("content", "Lucene offers powerful features through a simple API.", Field.Store.YES);
         doc.add(name);
         doc.add(url);
